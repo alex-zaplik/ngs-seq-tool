6fdab1ff88cc988566897f833ae5dc58431eb4e6
alex-zaplik <zaplikpl@gmail.com>
2019-10-05 17:05:21 +0200

    Extra arguments

--- backend/main.py
+++ backend/main.py
@@ -9,6 +9,8 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('--path', type=str, help="Path to the data file")
 parser.add_argument('--indexing', type=str, choices=("single", "double"), help="Indexing method. Expectig 'i7' if 'single' and 'i7' as well as 'i5' if 'double'")
+parser.add_argument('--runs', type=int, help="The number of sequencing runs")
+parser.add_argument('--samples', type=int, help="The number of DNA samples per run")
 
 args = parser.parse_args()
 
