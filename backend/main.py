b5ee9d9b4125ee81b434ebe3ad098f41ce05f1ff
alex-zaplik <zaplikpl@gmail.com>
2019-10-05 16:21:55 +0200

    This works now (kind of)

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 import algorithm as alg
 import argparse
+import customGenerator as cg
 
 from algorithmChecker import checkAlgorithm
 
@@ -7,19 +8,33 @@ from algorithmChecker import checkAlgorithm
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--path', type=str, help="Path to the data file")
-parser.add_argument('--indexing', type=str, choices=("single", "double"), help="Indexing method. Expectig i7 if 'single' and 'i7' as well as 'i5' if 'double'")
+parser.add_argument('--indexing', type=str, choices=("single", "double"), help="Indexing method. Expectig 'i7' if 'single' and 'i7' as well as 'i5' if 'double'")
 
 args = parser.parse_args()
 
 
-a = alg.BruteForce()
-res = a.group()
+runs = 6
+samples = 4
 
+i7 = cg.generateSequences(6 * 4, 10)
+
+a = alg.BruteForce(6, 4, i7)
+res = a.group()
 for r in res:
     for s in r:
         print(s)
     print()
 
-for r in res:
-    if not checkAlgorithm(r):
-        print("UPS! -> ", r)
+# i7 = cg.generateSequences(6, 10)
+# i5 = cg.generateSequences(4, 10)
+
+# a = alg.BruteForce(6, 4, i7, i5=i5)
+# res = a.group()
+# for r in res:
+#     for s in r:
+#         print(s)
+#     print()
+
+# for r in res:
+#     if not checkAlgorithm(r):
+#         print("UPS! -> ", r)
