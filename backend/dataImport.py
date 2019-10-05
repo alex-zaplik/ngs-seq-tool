b72bbf299bdb4d051ffbe292dfbac024bad3ba82
Mikołaj Pietrek <mikolajpietrek@pcQ21-guest.biol.uni.lodz.pl>
2019-10-05 14:42:01 +0200

    change file read method

--- backend/dataImport.py
+++ backend/dataImport.py
@@ -1,8 +1,13 @@
+import collections 
+
 def readNgsFile(filename, indexes):
     result = []
     with open(filename) as file:
+        header = file.readline().strip().split(" ")
         for line in file:
             lineList = line.strip().split(" ")
-            result.append(''.join([lineList[x] for x in indexes]))
+            resultDict = {header[x]:lineList[x] for x in indexes}
+            result.append(collections.namedtuple('Result', resultDict.keys())(**resultDict))
     print(result)
 
+readNgsFile("backend/data/indexy_illumina.txt", [2,3])
