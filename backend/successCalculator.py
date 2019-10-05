0c778c586439c2d0e099bb370e3cca6ed03d1830
Agata Zdunek <frealka@gmail.com>
2019-10-05 17:07:57 +0200

    succesCalculator fixed

--- backend/successCalculator.py
+++ backend/successCalculator.py
@@ -4,7 +4,7 @@ def estimateSuccessWithMonteCarlo(row :int, column :int):
     letters = 'ACTG'
     success = 0
     for i in range(1000):
-        word = random.choice(letters)
+        word = ''.join(random.choice(letters) for i in range(row))
         if word.__contains__('A') or (word.__contains__('C') and word.__contains__('T')):
             success += 1
     return pow(float(success)/1000,column)
