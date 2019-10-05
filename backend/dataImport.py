e124a4ad1bb2e5f92c5d9c2bb21441f6b7e61be9
Mikołaj Pietrek <mikolajpietrek@pcQ21-guest.biol.uni.lodz.pl>
2019-10-05 15:01:57 +0200

    file read 2nd bugfix

--- backend/dataImport.py
+++ backend/dataImport.py
@@ -3,11 +3,11 @@ import collections
 def readNgsFile(filename, indexes):
     result = []
     with open(filename) as file:
-        header = file.readline().strip().split(" ")
+        header = file.readline().replace("/","").strip().split(" ")
         for line in file:
             lineList = line.strip().split(" ")
             resultDict = {header[x]:lineList[x] for x in indexes}
             result.append(collections.namedtuple('Result', resultDict.keys())(**resultDict))
     return result
 
-print(readNgsFile("backend/data/indexy_illumina.txt", [2,3])[0])
+print(readNgsFile("backend/data/indexy_illumina.txt", [1,2,3,4])[0])
