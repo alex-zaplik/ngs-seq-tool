70c0585e6d199c319048211ff0b71d8265c56b0c
Mikołaj Pietrek <mikolajpietrek@localhost.localdomain>
2019-10-05 17:06:45 +0200

    change data import format

--- backend/dataImport.py
+++ backend/dataImport.py
@@ -4,10 +4,31 @@ def readNgsFile(filename, indexes):
     result = []
     with open(filename) as file:
         header = file.readline().replace("/","").strip().split(" ")
+        if not all(x in range(1, len(header)) for x in indexes):
+            raise Exception
         for line in file:
             lineList = line.strip().split(" ")
             resultDict = {header[x]:lineList[x] for x in indexes}
-            result.append(collections.namedtuple('Result', resultDict.keys())(**resultDict))
-    return result
+            resultNamedTuple = collections.namedtuple(lineList[0], resultDict.keys())(**resultDict)
+            result.append(resultNamedTuple)
+        return result
+        
 
-print(readNgsFile("backend/data/indexy_illumina.txt", [0,1,2,3,4])[0])
+def createStructure(resultList, column):
+    resDict = {}
+    for position in range(len(resultList[0][column])):
+        print(len(resultList[0][0]))
+        resDict[position] = {}
+        for code in ['C', 'A', 'T', 'G']:
+            resDict[position][code] = []
+            for ind, val in enumerate(resultList):
+                if val[column][position] == code:
+                    resDict[position][code].append(ind)
+    return resDict
+
+def createStructureFromFile(filename, column):
+    file = readNgsFile(filename, [column])
+    return createStructure(file, 0)
+
+result = createStructureFromFile("backend/data/indexy_illumina.txt", 3)
+print(result[2]["C"])
