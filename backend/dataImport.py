f31938529ff18535c2ce1e268958f9eca8ea1677
Mikołaj Pietrek <mikolajpietrek@pcQ21-guest.biol.uni.lodz.pl>
2019-10-05 15:25:11 +0200

    structure

--- backend/dataImport.py
+++ backend/dataImport.py
@@ -10,4 +10,4 @@ def readNgsFile(filename, indexes):
             result.append(collections.namedtuple('Result', resultDict.keys())(**resultDict))
     return result
 
-print(readNgsFile("backend/data/indexy_illumina.txt", [1,2,3,4])[0])
+print(readNgsFile("backend/data/indexy_illumina.txt", [0,1,2,3,4])[0])
