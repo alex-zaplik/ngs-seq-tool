491b8be4125ad355faed4b7487976887aefb822a
Merulo <damian.p.nowak@gmail.com>
2019-10-05 23:30:22 +0200

    more comments

--- backend/excerciseGenerator.py
+++ backend/excerciseGenerator.py
@@ -3,10 +3,10 @@ from customGenerator import generateCorrectSequences
 import argparse
 import sys
 
-
+# liczba grup, liczba sampli, dlugosc sampli
 generated = generateCorrectSequences(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
 
-print("Label i5_index")
+print("Label i5_index:")
 
 for i, gen in enumerate(generated):
     print("X%03d %s" % (i, gen))
