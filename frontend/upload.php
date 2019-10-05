d4416bbab700bc53d253beab601e50dc07df07b9
Mikołaj Pietrek <mikolajpietrek@localhost.localdomain>
2019-10-05 20:18:59 +0200

    css upload button

--- frontend/upload.php
+++ frontend/upload.php
@@ -19,16 +19,23 @@ move_not_logged_users($_SESSION);
     </head>
 
     <body class="h-100" style="background-color: #baed91;">
+
+        <div style="position: absolute; right:200px; bottom: 200px; transform: rotate(-20deg); color: #8cb36d;" class="h3">
+            thinking out of the box!
+        </div>
+
         <div class="container h-100">
             <div class="row h-100 justify-content-center align-items-center">
                 <form class="col-4 card" method="post" enctype="multipart/form-data">
                     <h1 class="my-3">File Upload</h1>
 
                     <div class="form-group">
-                        <input class="upload" type="file" name="files[]" id="file" multiple
-                            onchange="javascript:updateList()" />
+                        <div class="custom-file">
+                            <input type="file" class="custom-file-input" id="customFile" multiple>
+                            <label class="custom-file-label" for="customFile">Choose file</label>
+                        </div>
                     </div>
-
+                    
                     <div class="form-group">
                         <label for="formGroupExampleInput">Runs:</label>
                         <input type="text" class="form-control" id="runs" value="4" required>
@@ -67,15 +74,24 @@ move_not_logged_users($_SESSION);
                 </form> 
             </div>  
         </div>
-
-        <div style="position: absolute; right:200px; bottom: 200px; transform: rotate(-20deg); color: #8cb36d;" class="h3">
-            thinking out of the box!
-        </div>
     </body>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
+    <script>
+        $(".custom-file-input").on("change", function() {
+        if (document.getElementById("customFile").files.length == 1) {
+            var fileName = $(this).val().split("\\").pop();
+            console.log($(this).val().split("\\"));
+            $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+        } else {
+            text = document.getElementById("customFile").files.length + " files..."
+            console.log(text)
+            $(this).siblings(".custom-file-label").addClass("selected").html(text);
+        }
+        });
+    </script>
+
 </html>
-<script src="js/upload.js"></script>
