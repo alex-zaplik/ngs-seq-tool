a4e317603b160b57a03cc153b94bdbcf2c1aa0ca
alex-zaplik <zaplikpl@gmail.com>
2019-10-05 22:28:40 +0200

    Excercise generator

---
+++ backend/excerciseGenerator.py
@@ -0,0 +1,12 @@
+from customGenerator import generateCorrectSequences
+
+import argparse
+import sys
+
+
+generated = generateCorrectSequences(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
+
+print("Label i5_index")
+
+for i, gen in enumerate(generated):
+    print("X%03d %s" % (i, gen))
