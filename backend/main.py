13dab433f55d8ce0ab9b72be3134191ee37ee3b5
alex-zaplik <zaplikpl@gmail.com>
2019-10-05 15:23:43 +0200

    Started on the arguments parsing implementation

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,16 @@
 import algorithm as alg
+import argparse
 
 from algorithmChecker import checkAlgorithm
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--path', type=str, help="Path to the data file")
+parser.add_argument('--indexing', type=str, choices=("single", "double"), help="Indexing method. Expectig i7 if 'single' and 'i7' as well as 'i5' if 'double'")
+
+args = parser.parse_args()
+
 
 a = alg.BruteForce()
 res = a.group()
