4c9ba964b1a5198db89aedc8ed352228779342de
Agata Zdunek <frealka@gmail.com>
2019-10-06 03:53:47 +0200

    4-Channel algorithm checker implemented

--- backend/algorithmChecker.py
+++ backend/algorithmChecker.py
@@ -14,3 +14,21 @@ def checkAlgorithm(algorithmOutput, extraData= False):
         else:
             result.append(True)
     return result
+
+
+def checkAlgorithm_4Channel(algorithmOutput, extraData= False):
+    if not extraData:
+        for column in range(len(algorithmOutput[0])):
+            s = ''.join(row[column] for row in algorithmOutput)
+            if not (s.__contains__('A') and s.__contains__('C') and s.__contains__('T') and s.__contains__('G')):
+                return False
+        return True
+    
+    result = []
+    for column in range(len(algorithmOutput[0])):
+        s = ''.join(row[column] for row in algorithmOutput)
+        if not (s.__contains__('A') and s.__contains__('C') and s.__contains__('T') and s.__contains__('G')):
+            result.append(False)
+        else:
+            result.append(True)
+    return result
